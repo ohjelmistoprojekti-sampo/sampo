46f4ebb822070ab90125b7f504df18187d4ee92b
Olli Myyryläinen <ollimyy@users.noreply.github.com>
2023-09-12 12:02:45 +0300

    Initial commit: Create a basic landing page. #1 - Add placeholder text title and paragraph. - Add input field and backend processing for it. - Send input as placeholder response.

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,24 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
 
 const app = express();
+
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, Sampo-App');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'views')));
+
+app.get('/', (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
+
+app.post('/submit-description', (req: Request, res: Response) => {
+  const itemDescription: string = req.body.itemDescription;
+
+  /* Placeholder response to send itemDescription.
+   Change to redirect when the estimation form is done. */
+  res.send(itemDescription);
 });
 
 app.listen(port, () => {
