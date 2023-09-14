a17ff328f19ba63328702b91a8967fd31bd2a5e2
bgx160 <122889193+bgx160@users.noreply.github.com>
2023-09-14 10:02:00 +0000

    Add form submitting

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,8 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(express.static(path.join(__dirname, 'views')));
 
 app.get('/', (req: Request, res: Response) => {
@@ -15,7 +17,7 @@ app.get('/', (req: Request, res: Response) => {
 
 app.get('/questions', (req: Request, res: Response) => {
   const param = req.query.param;
-  let questions: {}[] = [{question: 'Tuotteen merkki ja malli?', answer: ''}, {question: 'Tuotteen kunto', answer: ''}];
+  let questions: {}[] = [{ question: 'Question1', answer: '' }, { question: 'Question2', answer: '' }];
 
   res.send(questions);
 })
@@ -27,6 +29,17 @@ app.post('/submit-description', (req: Request, res: Response) => {
    Change to redirect when the estimation form is done. */
   res.send(itemDescription);
 });
+
+app.post('/submit-form', (req: Request, res: Response) => {
+  const formData = req.body;
+  console.log(req.body);
+  const result = `
+  <div><h3>Vaihtoehto 1: Halpa ja nopea</h3><p>10 euroa</p></div>
+  <div><h3>Vaihtoehto 2: Keskihinta (suositeltu) </h3><p>30 euroa</p></div>  
+  <div><h3>Vaihtoehto 3: Kallis ja hidas</h3><p>50 euroa</p></div>`;
+  
+  res.send(result);
+})
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
