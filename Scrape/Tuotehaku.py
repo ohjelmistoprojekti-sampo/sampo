751fef5c35769af7aec2cee914af4f08fcad4c4f
SL9001 <samuli.lampela@outlook.com>
2023-09-18 15:09:54 +0300

    json data

--- Scrape/Tuotehaku.py
+++ Scrape/Tuotehaku.py
@@ -1,10 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
-
-
+import json
 
 if __name__ == "__main__":
-    
     # Take user input for the search term
     search_term = input("Tuottee nimi: ")
 
@@ -15,37 +13,36 @@ if __name__ == "__main__":
 
     soup = BeautifulSoup(page.content, "html.parser")
 
-
     results = soup.find(id="search-container")
 
-   
     hinta_lista = []
     nimi_lista = []
     kunto_lista = []
 
     job_elements = results.find_all("div", class_="item-card__title")
     hinta = results.find_all("div", class_="item-card__price item-card__price--buynow")
-    nimi = results.find_all("div",class_="item-card__title")
+    nimi = results.find_all("div", class_="item-card__title")
     kunto = results.find_all("div", class_="item-card__condition")
-    pairs = []
-
-# Assuming you have parsed the HTML and have BeautifulSoup objects hinta and nimi
-for hinta_elem, nimi_elem, kunto_elem in zip(hinta, nimi, kunto):
-    hinta_str = hinta_elem.get_text().replace(" ", "").replace("\n", "")
-    nimi_str = nimi_elem.get_text().strip()
-    kunto_str = kunto_elem.get_text().replace(" ", "").replace("\n", "")
-
     
-    # Create a pair and append it to the list
-    pair = [nimi_str, hinta_str, kunto_str]
-    pairs.append(pair)
+    data_list = []  # Create a list to store dictionaries for each item
 
-# Print the list of pairs
-for pair in pairs:
-    print("Nimi:", pair[0])
-    print("Hinta:", pair[1])
-    print("Kunto", pair[2])
-    print()
+    for hinta_elem, nimi_elem, kunto_elem in zip(hinta, nimi, kunto):
+        hinta_str = hinta_elem.get_text().replace(" ", "").replace("\n", "")
+        nimi_str = nimi_elem.get_text().strip()
+        kunto_str = kunto_elem.get_text().replace(" ", "").replace("\n", "")
 
+        # Create a dictionary for each item
+        item_data = {
+            "Nimi": nimi_str,
+            "Hinta": hinta_str,
+            "Kunto": kunto_str
+        }
+        
+        # Append the dictionary to the list
+        data_list.append(item_data)
 
-    
+    # Convert the list of dictionaries to JSON format
+    #json_data = json.dumps(data_list, indent=4)
+
+    # Print the JSON data (you can also save it to a file if needed)
+    print(data_list)
