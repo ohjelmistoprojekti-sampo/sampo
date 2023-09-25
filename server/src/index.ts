f26abb1b4f57aecc9e5b011b84f58c00ba28d172
bgx160 <122889193+bgx160@users.noreply.github.com>
2023-09-25 13:01:58 +0000

    Client side javascript to typescript #3

---
+++ server/src/index.ts
@@ -0,0 +1,40 @@
+import express, { Express, Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+const app: Express = express();
+
+const port = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/', express.static(path.join(__dirname, '..', 'client', 'public')))
+app.use('/dist', express.static(path.join(__dirname, '..', 'client', 'public')));
+
+app.get('/questions', (req: Request, res: Response) => {
+  const itemDescription = req.query.param;
+  console.log(itemDescription);
+  let questions: {}[] = [
+    { question: 'Missä kunnossa tuote on?', answer: '' },
+    { question: 'Kuinka vanha tuote on?', answer: '' },
+    { question: 'Minkä värinen tuote on?', answer: '' }
+  ];
+
+  res.send(questions);
+})
+
+app.post('/submit-form', (req: Request, res: Response) => {
+  const formData = req.body;
+  console.log(formData);
+
+  const prices: number[] = [10, 20, 30]
+
+  res.send(prices);
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
