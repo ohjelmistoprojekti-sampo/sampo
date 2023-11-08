7013bb89867e1cb72d5028c3b6f4668096a9e045
Topias <you@example.com>
2023-11-08 14:53:11 +0200

    Add database connection and query

--- server/src/dbAccess.ts
+++ server/src/dbAccess.ts
@@ -1,16 +1,34 @@
 import * as mongoDB from "mongodb";
+import dotenv from "dotenv";
 
-let itemCollection: mongoDB.Collection 
+dotenv.config();
+
+const DB_USER = process.env.DB_USER
+
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
+const encodedPassword = encodeURIComponent(DB_PASSWORD!);
+
+const DB_URL = process.env.DB_URL
+
+const CONNECTION_URL = `mongodb+srv://${DB_USER}:${encodedPassword}${DB_URL}`
+
+const DB_NAME = process.env.DB_NAME;
+
+const ITEM_COLLECTION = process.env.ITEM_COLLECTION;
+
+let itemCollection: mongoDB.Collection;
 
 export async function connectToDatabase() {
 
-    const client: mongoDB.MongoClient = new mongoDB.MongoClient(process.env.DB_URL!);
+    const client: mongoDB.MongoClient = new mongoDB.MongoClient(CONNECTION_URL);
 
     await client.connect();
 
-    const db: mongoDB.Db = client.db(process.env.DB_NAME);
+    const db: mongoDB.Db = client.db(DB_NAME);
 
-    itemCollection = db.collection(process.env.ITEM_COLLECTION!);
+    itemCollection = db.collection(ITEM_COLLECTION!);
+
 }
 
 export async function findItems(query: {}) {
@@ -22,6 +40,3 @@ export async function findItems(query: {}) {
         throw error;
     }
 }
-
-let query = {title: "ruokapöytä"};
-console.log(findItems(query));
